8def8b3752ae82c47b2b69935ea78422aa796b9a
Luiz Henrique Pegnolatto <lhpegnolatto@gmail.com>
2026-04-03 08:11:56 -0300

    initial commit

---
+++ apps/shell/src/App.tsx
@@ -0,0 +1,7 @@
+export function App() {
+  return (
+    <main className="flex min-h-screen items-center justify-center bg-zinc-950 text-zinc-100">
+      <h1 className="text-3xl font-bold underline">shell</h1>
+    </main>
+  );
+}
