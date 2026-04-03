75f242631ae4e6d8be6354964287f8dc35b5539a
Luiz Henrique Pegnolatto <lhpegnolatto@gmail.com>
2026-04-03 10:50:23 -0300

    feat(analytics): add shadcn and mocked dash

--- apps/analytics/src/exposed/app.tsx
+++ apps/analytics/src/exposed/app.tsx
@@ -1,9 +1,24 @@
+import { ChartAreaInteractive } from "@/components/chart-area-interactive";
+import { DataTable } from "@/components/data-table";
+import { SectionCards } from "@/components/section-cards";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import data from "@/mocks/data.json";
 import "@/styles/globals.css";
 
 export default function App() {
   return (
     <main>
-      <h1 className="text-2xl font-bold text-amber-200">analytics app 2</h1>
+      <TooltipProvider>
+        <div className="@container/main flex flex-1 flex-col gap-2">
+          <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
+            <SectionCards />
+            <div className="px-4 lg:px-6">
+              <ChartAreaInteractive />
+            </div>
+            <DataTable data={data} />
+          </div>
+        </div>
+      </TooltipProvider>
     </main>
   );
 }
